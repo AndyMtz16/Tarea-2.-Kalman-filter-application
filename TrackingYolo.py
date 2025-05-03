23c011af1a6bfb461f308c896cb9365a5977cf10
AndyMtz16 <166466775+AndyMtz16@users.noreply.github.com>
2025-05-02 19:44:45 -0600

    Update TrackingYolo.py
    Código limpio con la implementación de Tracker

--- TrackingYolo.py
+++ TrackingYolo.py
@@ -5,12 +5,10 @@ from sort import convert_x_to_bbox
 import supervision as sv
 from trackers import SORTTracker
 
-#torch.backends.cudnn.enabled = False
 
 VIDEO   = "KALMAN_FILTER\gatos_yolo.mp4"
 model   = YOLO("yolov8m.pt")
 tracker = SORTTracker()
-#tracker = Sort(max_age=30, min_hits=15, iou_threshold=0.3)
 annotator = sv.LabelAnnotator(text_position=sv.Position.CENTER)
 
 
@@ -26,39 +24,3 @@ sv.process_video(
     callback=callback,
 )
 
-"""
-cap = cv2.VideoCapture(VIDEO)
-while True:
-    ok, frame = cap.read()
-    if not ok:
-        print("Fin del video"); break
-
-    # detección
-    res  = model(frame, conf=0.65, verbose=False)[0]
-    dets = res.boxes.data.cpu().numpy()[:, :5]if res.boxes else np.empty((0, 5))
-
-    # tracking
-    for x1, y1, x2, y2, _, tid in tracker.update(dets).astype(int):
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,0), 2)
-        cv2.putText(frame, f'ID {tid}', (x1, y1-5),
-                cv2.FONT_HERSHEY_SIMPLEX, .6, (0,255,0), 2)
-
-    # pred Kalman (caja amarilla)
-    for trk in tracker.trackers:
-        px1, py1, px2, py2 = convert_x_to_bbox(trk.kf.x)[0].astype(int)
-
-        cv2.rectangle(frame, (px1, py1), (px2, py2),
-                    (0, 255, 255), 1, cv2.LINE_4)
-
-        cv2.putText(frame, f'ID {trk.id + 1} (pred)',
-                    (px1, py1 - 6),               
-                    cv2.FONT_HERSHEY_SIMPLEX, .5,
-                    (0, 255, 255), 1)
-
-
-    cv2.imshow("YOLOv8 + Kalman SORT", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release(); cv2.destroyAllWindows()
-"""
